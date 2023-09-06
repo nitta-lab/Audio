60dda161b665d28089db1f34fafa97938fb1394b
Okina-kina <Okinakinako@gmail.com>
2023-09-06 21:27:37 +0900

    add test cases

---
+++ lib/src/test/java/AudioTestBeforeRefactoring.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AudioTestBeforeRefactoring {
+    @Test void someLibraryMethodReturnsTrue() {
+        Audio audio = new Audio();
+        audio.setVolume(0.4);
+        
+        //
+    }
+}
